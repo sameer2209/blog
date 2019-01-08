907360a0ffc00dab07964314e80dc6d5b20ee8f7
Sameer Soin <sameersoin95@gmail.com>
2019-01-08 15:42:59 +0530

    Article create edit show delete implemented

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,57 @@
+class ArticlesController < ApplicationController
+  def index
+  	@articles = Article.all
+  end
+
+  def show
+  	@article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+    	flash[:notice] = "Article was successfully created"
+    	redirect_to article_path(@article)
+    else
+    	render('new')
+    end
+    
+  end
+
+  def edit
+  	@article = Article.find(params[:id])
+  end
+
+  def update
+  	@article = Article.find(params[:id])
+  	if @article.update_attributes(article_params)
+  		flash[:notice] = "Article was successfully updated"
+  		redirect_to article_path(@article)
+  	else
+  		render 'edit'
+  	end
+  end
+
+  def destroy
+  	@article = Article.find(params[:id])
+  	
+  	if @article.destroy
+  		flash[:notice] = "Article '#{@article.title}' deleted successfully."
+  		redirect_to articles_path
+  	else
+  		flash[:notice] = "Error occurred"
+  	end
+
+  end
+
+  private
+
+  	def article_params
+  		params.require(:article).permit(:title, :content)
+  	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  # get 'articles/index'
+  # get 'articles/show'
+  # get 'articles/new'
+  # get 'articles/edit'
+  # get 'articles/delete'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ root 'pages#index'
+ 
+ get 'about', to: 'pages#about'
+
+ resources :articles
+  
 end
