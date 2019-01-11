2a51447a71c9952077382aa51822176f09d39b1a
Sameer Soin <sameersoin95@gmail.com>
2019-01-11 13:06:52 +0530

    Comment creation and deletion using ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,46 @@
 class CommentsController < ApplicationController
 
-	before_action :authenticate_user!
+	# before_action :authenticate_user!, only: [:create]
 
 	def create
-		# render json: params[:comment][:content]
-		@comment = Comment.new(:content => params[:comment][:content])
-		@comment.article_id = params[:article_id]
-		@comment.user_id = current_user.id
-		@comment.save!
+		# render json: "my name is khan"
+		respond_to do |format|
+			if current_user.present?
+				@comment = Comment.new(:content => params[:comment][:content])
+				@comment.article_id = params[:article_id]
+				@article = Article.find(params[:article_id])
+				@comment.user_id = current_user.id
+				if @comment.save!
+					format.html {redirect_to article_path(@comment.article)}
+					#format.html {puts "inside 1"}
+					format.js
+					#format.js{puts "inside 2"}
+				else
+					format.html {render 'articles/comment'}
+					#format.html {puts "inside 3"}
+					format.js
+					#format.js{puts "inside 4"}
+				end
+			else
+				format.js
+			end
+		end	
+		#redirect_to article_path(@comment.article)
+	end
 
-		redirect_to article_path(@comment.article)
+	def destroy
+		@comment = Comment.find(params[:id])
+		@article = Article.find(params[:article_id])
+		respond_to do |format|
+			
+			if @comment.destroy
+				format.html {redirect_to article_path(@comment.article)}
+				format.js
+			else
+
+			end
+		end
+
 	end
 
 
